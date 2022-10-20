cacdcc8971fa6d7b316a4499cb89205396c4b868
MateoCoria <MateoChristianCoria@gmail.com>
2022-10-20 13:50:26 -0300

    busqueda de interprete para listar, Mateo Coria

--- disqueria/views.py
+++ disqueria/views.py
@@ -25,6 +25,18 @@ class ProyectoIntegrador(TemplateView):
     template_name = "crud/proyecto.html"
 
 
+def buscar_interprete(request):
+    if 'q' in request.GET:
+        q = request.GET['q']
+        all_album_list = Album.objects.filter(interprete__nombre__icontains=q)).order_by('interprete') 
+            
+    else:
+        all_album_list = Album.objects.all().order_by('interprete')                
+        
+
+    return render(request, 'buscar.html', {"album":all_album_list})
+
+
 def buscar(request):
     if 'q' in request.GET:
         q = request.GET['q']
